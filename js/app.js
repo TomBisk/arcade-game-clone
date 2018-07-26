a56e8f901d31973546ccc7f9e686eae5b7f995fa
Tomasz Bochenek <bochenekt@gmail.com>
2018-07-27 00:10:09 +0200

    Add general player class and variable

--- js/app.js
+++ js/app.js
@@ -24,13 +24,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+function Player() {
+	
+	//define player's figure image
+	this.player = "img/char-boy.png";
+};
+
+Player.prototype.update = function update() {
+	
+};
+
+Player.prototype.render = function render() {
+	
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [];
 // Place the player object in a variable called player
-
+const player = new Player();
 
 
 // This listens for key presses and sends the keys to your
